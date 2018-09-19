54a754e809ca672ec2d4f7bf514eb2ced963d62a
Weskley Bezerra <weskleymb@hotmail.com>
2018-09-19 20:55:43 -0300

    adicao de Departamento e Produto

---
+++ src/br/senac/rn/loja/model/Departamento.java
@@ -0,0 +1,72 @@
+package br.senac.rn.loja.model;
+
+public class Departamento implements Comparable<Departamento> {
+
+	private Integer id;
+	private String nome;
+	private String sigla;
+	private Float desconto;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSigla() {
+		return sigla;
+	}
+	
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
+	public Float getDesconto() {
+		return desconto;
+	}
+	
+	public void setDesconto(Float desconto) {
+		this.desconto = desconto;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public int compareTo(Departamento departamento) {
+		return id.compareTo(departamento.id);
+	}
+	
+}
---
+++ src/br/senac/rn/loja/model/Produto.java
@@ -0,0 +1,72 @@
+package br.senac.rn.loja.model;
+
+public class Produto implements Comparable<Produto> {
+
+	private Integer id;
+	private String nome;
+	private Float preco;
+	private Departamento departamento;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Float getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+	
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+	
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public int compareTo(Produto produto) {
+		return id.compareTo(produto.id);
+	}
+	
+}
