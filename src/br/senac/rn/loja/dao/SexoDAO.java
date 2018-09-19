6fa9aa3c6c8e7daf6765fc12d0e289931268bb46
Weskley Bezerra <weskleymb@hotmail.com>
2018-09-18 21:06:07 -0300

    commit inicial

--- src/br/senac/rn/loja/dao/SexoDAO.java
+++ src/br/senac/rn/loja/dao/SexoDAO.java
@@ -1,53 +1,65 @@
 package br.senac.rn.loja.dao;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
-
 import br.senac.rn.loja.model.Sexo;
 
 public class SexoDAO {
+	
+	//MariaDB -> org.mariadb.jdbc.Driver
+	//MySQL -> com.mysql.cj.jdbc.Driver
+	//PostgreSQL -> org.postgresql.Driver
+	//MSSQL -> com.microsoft.sqlserver.jdbc.SQLServerDriver
 
-	private static List<Sexo> sexos = new ArrayList<Sexo>();
+	private final String DRIVER = "org.mariadb.jdbc.Driver";
+	private final String BANCO = "db_loja_manha";
+	private final String URL = "jdbc:mariadb://localhost:3306/" + BANCO;
+	private final String USUARIO = "root";
+	private final String SENHA = "";
 	
-	public void inserir(Sexo sexo) {
-		sexos.add(sexo);
-	}
-	
-	public void remover(Sexo sexo) {
-		sexos.remove(sexo);
-	}
-	
-	public void editar(Sexo sexo) {
-		int indice = sexos.indexOf(sexo);
-		sexos.set(indice, sexo);
-	}
-	
-	public List<Sexo> buscaTodos() {
-		return sexos;
-	}
-	
-	public Sexo buscaPorId(Integer id) {
-		Sexo sexo = new Sexo();
-		sexo.setId(id);
-		int indice = sexos.indexOf(sexo);
-		return sexos.get(indice);
-	}
-	
-	public Sexo buscaPorNome(String nome) {
-		for (Sexo sexo : sexos) {
-			if (sexo.getNome().equals(nome)) {
-				return sexo;
-			}
+	private Connection getConexao() {
+		try {
+			Class.forName(DRIVER);
+			Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA);
+			return connection;
+		} catch (ClassNotFoundException exception) {
+			System.out.println("DRIVER NAO ENCONTRADO!!!");
+		} catch (SQLException exception) {
+			System.out.println("PROBLEMAS COM A CONEXAO!!!");
 		}
 		return null;
 	}
 	
-	public Sexo buscaPorSigla(String sigla) {
-		for (Sexo sexo : sexos) {
-			if (sexo.getSigla().equals(sigla)) {
-				return sexo;
-			}
+	public void inserir(Sexo sexo) {
+		String sql = "INSERT INTO tb_sexos (sex_nome, sex_sigla) VALUES (?, ?)";
+		try {
+			PreparedStatement statement = getConexao().prepareStatement(sql);
+			statement.setString(1, sexo.getNome());
+			statement.setString(2, sexo.getSigla());
+			statement.executeUpdate();
+		} catch (SQLException exception) {
+			System.out.println("ERRO: " + exception.getMessage());
 		}
+	} 
+	
+	public void remover(Sexo sexo) {
+		//to do
+	}
+	
+	public void editar(Sexo sexo) {
+		//to do
+	}
+	
+	public List<Sexo> buscaTodos() {
+		//to do
+		return null;
+	}
+	
+	public Sexo buscaPorId(Integer id) {
+		//to do
 		return null;
 	}
 	
--- src/br/senac/rn/loja/teste/TestaArrayList.java
+++ src/br/senac/rn/loja/teste/TestaArrayList.java
@@ -1,6 +1,5 @@
 package br.senac.rn.loja.teste;
 
-import java.util.Scanner;
 import br.senac.rn.loja.dao.SexoDAO;
 import br.senac.rn.loja.model.Sexo;
 
@@ -8,31 +7,38 @@ public class TestaArrayList {
 
 	public static void main(String[] args) {
 
-		Scanner entrada = new Scanner(System.in);
-
+//		Scanner entrada = new Scanner(System.in);
+//
+//		SexoDAO dao = new SexoDAO();
+//		
+//		int resposta;
+//		
+//		do {
+//			Sexo sexo = new Sexo();
+//			System.out.println("Informe o nome do Sexo:");
+//			sexo.setNome(entrada.next());
+//			System.out.println("Informe a sigla do Sexo:");
+//			sexo.setSigla(entrada.next());
+//			dao.inserir(sexo);
+//			System.out.println("Digite 0 (ZERO) para SAIR");
+//			resposta = entrada.nextInt();
+//		} while(resposta != 0);
+//		
+//		Sexo sm = new Sexo();
+//		sm.setId(1);
+//		sm.setNome("teste");
+//		dao.editar(sm);
+//		
+//		dao.buscaTodos().forEach(sexo -> System.out.println(sexo));
+//		
+//		entrada.close();
+		
+		Sexo s = new Sexo();
+		s.setNome("teste");
+		s.setSigla("t");
+		
 		SexoDAO dao = new SexoDAO();
-		
-		int resposta;
-		
-		do {
-			Sexo sexo = new Sexo();
-			System.out.println("Informe o nome do Sexo:");
-			sexo.setNome(entrada.next());
-			System.out.println("Informe a sigla do Sexo:");
-			sexo.setSigla(entrada.next());
-			dao.inserir(sexo);
-			System.out.println("Digite 0 (ZERO) para SAIR");
-			resposta = entrada.nextInt();
-		} while(resposta != 0);
-		
-		Sexo sm = new Sexo();
-		sm.setId(1);
-		sm.setNome("teste");
-		dao.editar(sm);
-		
-		dao.buscaTodos().forEach(sexo -> System.out.println(sexo));
-		
-		entrada.close();
+		dao.inserir(s);
 				
 	}
 
